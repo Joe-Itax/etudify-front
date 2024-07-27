d2e7b92d2142ae348c36375f482ba8cc31cb18c4
Joseph Itakala <josephitakala18@gmail.com>
2024-07-27 01:01:35 +0100

    feat: add a robots and sitemap files

---
+++ src/app/sitemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://etudiant3-0.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    // {
+    //   url: "https://genius-service.vercel.app/services",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.8,
+    // },
+    // {
+    //   url: "https://genius-service.vercel.app/realisations",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.8,
+    // },
+    // {
+    //   url: "https://genius-service.vercel.app/about",
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly",
+    //   priority: 0.6,
+    // },
+    // {
+    //   url: "https://genius-service.vercel.app/contact",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.7,
+    // },
+    // {
+    //   url: "https://acme.com/blog",
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly or monthly or yearly",
+    //   priority: 0.5,
+    // },
+  ];
+}
