ce9b81d28212b96b8bdbca06cc1dfebf40c272b7
Joseph Itakala <josephitakala18@gmail.com>
2025-02-07 16:50:09 +0100

    fix error deploiment

--- src/app/(pages)/ressources/page.tsx
+++ src/app/(pages)/ressources/page.tsx
@@ -1,6 +1,5 @@
-"use client";
-
 import { Search } from "@/components/search.component";
+import { Suspense } from "react";
 
 interface Props {
   params: {
@@ -15,7 +14,9 @@ export default function Page({ params }: Props) {
     <>
       <h1>Page {id}</h1>
       <p>Page content</p>
-      <Search placeholder="Rechercher une Ressource (ex. Math, Science, etc...)" />
+      <Suspense fallback={<div>Chargement...</div>}>
+        <Search placeholder="Rechercher une Ressource (ex. Math, Science, etc...)" />
+      </Suspense>
     </>
   );
 }
